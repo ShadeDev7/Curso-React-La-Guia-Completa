dff41b83767e5c7bf4605158816a7861b86e4dd4
ShadeDev7 <ShadeDev7@gmail.com>
2022-06-14 00:08:32 -0300

    Revert "151 - 186"
    This reverts commit 6253fa49208f4154ff38119fc7c09eb9ca22afa9.

--- Administrador de Clientes/src/components/ErrorAlert.jsx
+++
@@ -1,5 +0,0 @@
-const ErrorAlert = ({ children }) => {
-    return <div className="text-center my-4 bg-red-600 text-white text-sm font-bold p-1 uppercase">{children}</div>;
-};
-
-export default ErrorAlert;
